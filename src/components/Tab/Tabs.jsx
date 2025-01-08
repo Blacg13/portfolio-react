d2d4802edd2639069ebf033af81b8b051d68f5ca
Bénédicte Lagasse <benedicte.lagasse@proton.me>
2025-01-08 16:54:23 +0100

    🚀 tabs initiated

--- src/components/Tab/Tabs.jsx
+++ src/components/Tab/Tabs.jsx
@@ -3,24 +3,29 @@ import Tab from './Tab/Tab';
 const Tabs = () => {
   const tabs = [
     {
-    tabTopic: 'about',
-    tabTitle: 'about me',
-    tabContent: [
+      tabTopic: 'about',
+      tabTitle: 'about me',
+      tabContent: [
         "I'm passionate about programming and designing intuitive website interfaces",
-        "Currently training to become a frontenddeveloper, I'm diving deep into JavaScript and React with a relentless curiosity to master and optimize every line of code."],
-    githubTabLink: null,
-    liveTabLink: null,},
-    {
-    tabTopic: 'projects',
-    tabTitle: 'projects',
-    tabContent: [
-            "bloub", "blabb"
-        ],
-        githubTabLink: null,
-        liveTabLink: null,
-    }
+        "Currently training to become a frontenddeveloper, I'm diving deep into JavaScript and React with a relentless curiosity to master and optimize every line of code.",
       ],
+      githubTabLink: null,
+      liveTabLink: null,
+    },
+    {
+      tabTopic: 'projects',
+      tabTitle: 'projects',
+      tabContent: ['bloub', 'blabb'],
+      githubTabLink: null,
+      liveTabLink: null,
     },
   ];
+
+  return (
+    <div>
+      <p>hello</p>
+      <Tab />
+    </div>
+  );
 };
 export default Tabs;
