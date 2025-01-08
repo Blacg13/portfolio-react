bc6b56b897ef546a5e7c8b7759dcef35ca23c1e7
Bénédicte Lagasse <benedicte.lagasse@proton.me>
2025-01-08 16:52:53 +0100

    🚀 tabs initiated

---
+++ src/components/Tab/Tabs.jsx
@@ -0,0 +1,26 @@
+import Tab from './Tab/Tab';
+
+const Tabs = () => {
+  const tabs = [
+    {
+    tabTopic: 'about',
+    tabTitle: 'about me',
+    tabContent: [
+        "I'm passionate about programming and designing intuitive website interfaces",
+        "Currently training to become a frontenddeveloper, I'm diving deep into JavaScript and React with a relentless curiosity to master and optimize every line of code."],
+    githubTabLink: null,
+    liveTabLink: null,},
+    {
+    tabTopic: 'projects',
+    tabTitle: 'projects',
+    tabContent: [
+            "bloub", "blabb"
+        ],
+        githubTabLink: null,
+        liveTabLink: null,
+    }
+      ],
+    },
+  ];
+};
+export default Tabs;
